e83ec4d8614f0a73f4ccf9a44c3fc3f359fee592
Mateus Almeida <mateusalmeida071@icloud.com>
2020-12-03 19:00:53 -0300

    Update

--- simpsons-rule.c
+++ simpsons-rule.c
@@ -10,27 +10,27 @@ double function (double x) {
 // This function calculates the approximation of an 
 // integral using Simpson's Rule
 double simpsonsRule (double a, double b, unsigned int n) {
-  double X0[n], Y0[n], I = 0;
+  double x0[n], y0[n], I = 0;
 
   int i, j;
 
-  double H = (b - a) / n;
+  double h = (b - a) / n;
 
   for(i = 0; i <= n; i++) {
-    X0[i] = a + H * i;
-    Y0[i] = function(X0[i]);
+    x0[i] = a + h * i;
+    y0[i] = function(x0[i]);
   }
 
   for(i = 1; i < n; i++) {
     if(i % 2 == 0){
-      I += 2 * Y0[i];
+      I += 2 * y0[i];
     } else {
-      I += 4 * Y0[i];
+      I += 4 * y0[i];
     }
   }
 
-  I += Y0[0] + Y0[n];
-  I = (H / 3) * I;
+  I += y0[0] + y0[n];
+  I = (h / 3) * I;
 
   return I;
 }
@@ -39,7 +39,7 @@ double simpsonsRule (double a, double b, unsigned int n) {
 int main () {
   // Integration limits
   double a = 5, b = 3;
-  // Subinterval, only positive values
+  // Subinterval
   unsigned int n = 2;
 
   double I = simpsonsRule(a, b, n);
